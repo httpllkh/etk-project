1e075066486d8c1442d0db561d862981a08ffc8a
httpllkh <72464781+httpllkh@users.noreply.github.com>
2025-03-11 21:41:33 +0500

    https Update

--- src/components/modals/AddModal.jsx
+++ src/components/modals/AddModal.jsx
@@ -18,7 +18,7 @@ const AddModal = ({ active, setActive, onVehicleAdded }) => {
 
   const handleSaveAndClose = async () => {
     try {
-      await fetch('http://91.203.10.130:2783/portal/hs/ksapi/newts', {
+      await fetch('https://etk861c.ru:2784/portal/hs/ksapi/newts', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
