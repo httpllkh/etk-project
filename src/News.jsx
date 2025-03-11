9fb16f539b739e1065c38c588c97a5124e89bcae
httpllkh <72464781+httpllkh@users.noreply.github.com>
2025-03-11 21:37:53 +0500

    https Update

--- src/News.jsx
+++ src/News.jsx
@@ -13,7 +13,7 @@ const Vehicle = () => {
 
   const fetchNews = async () => {
     try {
-      const response = await fetch('http://91.203.10.130:2783/portal/hs/ksapi/GETNEWS', {
+      const response = await fetch('https://etk861c.ru:2784/portal/hs/ksapi/GETNEWS', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
